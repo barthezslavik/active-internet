d110f59652025af683f5d588bc7213c277b0b221
Viacheslav Loginov <barthez.slavik@gmail.com>
2025-09-30 00:49:06 +0300

    Auto

--- background.js
+++ background.js
@@ -31,6 +31,30 @@ chrome.action.onClicked.addListener((tab) => {
 
       // Send a message to the content script to update its state
       chrome.tabs.sendMessage(tab.id, { isActive: newState });
+
+      // If deactivating, set an alarm to reactivate after 1 minute
+      if (!newState) {
+        chrome.alarms.create('reactivate', { delayInMinutes: 1 });
+      } else {
+        // If activating, clear the reactivation alarm
+        chrome.alarms.clear('reactivate');
+      }
     });
   });
+});
+
+// Listen for the reactivation alarm
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'reactivate') {
+    chrome.storage.local.set({ isActive: true }, () => {
+      updateBadge();
+
+      // Notify all tabs about the reactivation
+      chrome.tabs.query({}, (tabs) => {
+        tabs.forEach(tab => {
+          chrome.tabs.sendMessage(tab.id, { isActive: true }).catch(() => {});
+        });
+      });
+    });
+  }
 });
